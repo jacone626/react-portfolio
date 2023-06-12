c86df42483770870586d502d2b152a40caca3f69
James A Cone <jacone626@gmail.com>
2023-06-12 17:20:29 -0400

    added some html for portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,7 +3,50 @@ import React from 'react';
 
 function Portfolio() {
   return (
-    <h1>Portfolio</h1>
+  <div>
+    <section id="portfolio" class="portfolio">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Portfolio</h2>
+        <p>My Works</p>
+      </div>
+
+      <div class="row portfolio-container">
+
+        <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+          <div class="portfolio-wrap">
+            <img src="assets/images/crypto-project-1.png" class="img-fluid" alt="crypto lookup application homescreen"/>
+            <div class="portfolio-info">
+              <h4>Crypto-Look-up</h4>
+              <p>HTML/CSS/Javscript</p>
+              <div class="portfolio-links">
+                <a href="https://github.com/A-Witthohn/Group-3-Crypto-Price-Project-1" target="_blank"><p class="portfolio-href">Github Repo</p></a>
+                <a href="https://a-witthohn.github.io/Group-3-Crypto-Price-Project-1/" target="_blank"><p class="portfolio-href">Project link</p></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+          <div class="portfolio-wrap">
+            <img src="assets/images/strain-finder.png" class="img-fluid" alt="strain finder reviews page"/>
+            <div class="portfolio-info">
+              <h4>Strain Finder</h4>
+              <p>Javascript/Node.js/Sequelize</p>
+              <div class="portfolio-links">
+                <a href="https://github.com/A-Witthohn/Strain-Finder" target="_blank"><p class="portfolio-href">Github Repo</p></a>
+                <a href="https://strain-finder-app.herokuapp.com/" target="_blank"><p class="portfolio-href">Project Link</p></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+</div>
   )
 }
 
