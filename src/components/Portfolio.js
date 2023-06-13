0798db167bf2057014638cbd257373bc4f77dde0
James A Cone <jacone626@gmail.com>
2023-06-13 16:32:08 -0400

    added a projects object that holds all of the information for each project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,51 @@
 import React from 'react';
 import testPhoto from "../images/crypto-project-1.png"
 
+const projects = [
+  {
+    title: "Crypto Lookup",
+    technologies: "HTML/CSS/Javascript",
+    image: "../images/crypto-project-1.png"
+    github: "https://github.com/A-Witthohn/Group-3-Crypto-Price-Project-1",
+    link: "https://a-witthohn.github.io/Group-3-Crypto-Price-Project-1/",
+  },
+  {
+    title: "Strain Finder",
+    technologies: "Javascript/Node.js/Sequelize",
+    image: "../images/strain-finder.png"
+    github: "https://github.com/A-Witthohn/Strain-Finder",
+    link: "https://strain-finder-app.herokuapp.com/",
+  },
+  {
+    title: "Weather Dashboard",
+    technologies: "APIs/Bootstrap/Javascript",
+    image: "../images/weather-dashboard-wilmington.png"
+    github: "https://github.com/jacone626/weather-dashboard",
+    link: "https://jacone626.github.io/weather-dashboard/",
+  },
+  {
+    title: "Tech Blog",
+    technologies: "MVC/MySQL/Node.js",
+    image: "../homepage.png"
+    github: "https://github.com/jacone626/tech-blog",
+    link: "https://tech-blog-jacone626.herokuapp.com/",
+  },
+  {
+    title: "Note Taker",
+    technologies: "Heroku/Express.js/Javascript",
+    image: "../images/note-taker-home.png"
+    github: "https://github.com/jacone626/note-taker",
+    link: "https://secure-plains-21587.herokuapp.com/",
+  },
+  {
+    title: "Work Day Scheduler",
+    technologies: "Local Storage/jQuery/Day.js",
+    image: "../images/Work-day-scheduler.png"
+    github: "https://github.com/jacone626/work-day-scheduler",
+    link: "https://jacone626.github.io/work-day-scheduler/",
+  },
+]
+
 function Portfolio() {
   return (
     <div className="container mt-5">
