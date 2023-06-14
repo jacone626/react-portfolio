36c7827bbe4c838aed7250fa6163b882e4740216
James A Cone <jacone626@gmail.com>
2023-06-14 10:29:25 -0400

    got pictures to render and added styling to page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,47 +1,47 @@
 import React from 'react';
 import Project from "./Project";
-import testPhoto from "../images/crypto-project-1.png";
+import '../css/Portfolio.css';
 
 const projects = [
   {
     title: "Crypto Lookup",
     technologies: "HTML/CSS/Javascript",
-    image: "../images/crypto-project-1.png",
+    image: "/images/crypto-project-1.png",
     github: "https://github.com/A-Witthohn/Group-3-Crypto-Price-Project-1",
     link: "https://a-witthohn.github.io/Group-3-Crypto-Price-Project-1/",
   },
   {
     title: "Strain Finder",
     technologies: "Javascript/Node.js/Sequelize",
-    image: "../images/strain-finder.png",
+    image: "/images/strain-finder.png",
     github: "https://github.com/A-Witthohn/Strain-Finder",
     link: "https://strain-finder-app.herokuapp.com/",
   },
   {
     title: "Weather Dashboard",
     technologies: "APIs/Bootstrap/Javascript",
-    image: "../images/weather-dashboard-wilmington.png",
+    image: "/images/weather-dashboard-wilmington.png",
     github: "https://github.com/jacone626/weather-dashboard",
     link: "https://jacone626.github.io/weather-dashboard/",
   },
   {
     title: "Tech Blog",
     technologies: "MVC/MySQL/Node.js",
-    image: "../homepage.png",
+    image: "/images/homepage.png",
     github: "https://github.com/jacone626/tech-blog",
     link: "https://tech-blog-jacone626.herokuapp.com/",
   },
   {
     title: "Note Taker",
     technologies: "Heroku/Express.js/Javascript",
-    image: "../images/note-taker-home.png",
+    image: "/images/note-taker-home.png",
     github: "https://github.com/jacone626/note-taker",
     link: "https://secure-plains-21587.herokuapp.com/",
   },
   {
     title: "Work Day Scheduler",
     technologies: "Local Storage/jQuery/Day.js",
-    image: "../images/Work-day-scheduler.png",
+    image: "/images/Work-day-scheduler.png",
     github: "https://github.com/jacone626/work-day-scheduler",
     link: "https://jacone626.github.io/work-day-scheduler/",
   },
@@ -54,22 +54,6 @@ function Portfolio() {
      
         <Project projects={projects}/>
 
-      <div className="row portfolio-container">
-
-        <div className="col-lg-4 col-md-6 portfolio-item filter-app">
-          <div className="portfolio-wrap">
-            <img src={testPhoto} className="img-fluid" alt="crypto lookup application homescreen"/>
-            <div className="portfolio-info">
-              <h4>Crypto-Look-up</h4>
-              <p>HTML/CSS/Javscript</p>
-              <div className="portfolio-links">
-                <a href="https://github.com/A-Witthohn/Group-3-Crypto-Price-Project-1" target="_blank" rel="noreferrer"><p className="portfolio-href">Github Repo</p></a>
-                <a href="https://a-witthohn.github.io/Group-3-Crypto-Price-Project-1/" target="_blank" rel="noreferrer"><p className="portfolio-href">Project link</p></a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   )
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,15 +1,16 @@
 import React from "react";
+import '../css/Portfolio.css'
 
 function Project(props) {
     return (
         <div className="row portfolio-container">
         {props.projects.map(project => (   
         <div className="col-lg-4 col-md-6 portfolio-item filter-app" key={project.id}>
-          <div className="portfolio-wrap">
-            <img src={project.image} className="img-fluid" alt="application homepage"/>
+          <div className="portfolio-wrap portfolio-background">
+            <img src={project.image} className="img-fluid" alt="application homepage" id="portfolio-images"/>
             <div className="portfolio-info">
-              <h4>Name: {project.title}</h4>
-              <p>Tech: {project.technologies}</p>
+              <h4>{project.title}</h4>
+              <p>Technologies: {project.technologies}</p>
               <div className="portfolio-links">
                 <a href={project.github} target="_blank" rel="noreferrer"><p className="portfolio-href">Github Repo</p></a>
                 <a href={project.link} target="_blank" rel="noreferrer"><p className="portfolio-href">Project link</p></a>
